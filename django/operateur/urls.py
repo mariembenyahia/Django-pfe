a404948cd0035acbc7a55917cd0d7836f4ddf0c5
mariembenyahia <123958248+mariembenyahia@users.noreply.github.com>
2024-05-18 11:43:06 +0200

    Add files via upload

---
+++ django/operateur/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+
+
+    path('signup/' ,views.SignupPage,name='SignupPage'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profil/', views.profil, name='profil'),
+    path('loginoperateur/',views.login_view2,name='login_view2'),
+    path('dashboardop/',views.dashboardop,name='dashboardop'),
+    path('Customers/',views.Customers,name='Customers'),
+    path('profilop/', views.profilop, name='profilop'),
+    path('claim/', views.claim, name='claim'),
+    path('claimop/', views.claimop, name='claimop'),
+    path('fortigate/', views.fortigate, name='fortigate'),  
+    path('routestatic/', views.routestatic, name='routestatic'),
+    path('sdwan/', views.sdwan, name='sdwan'),
+    path('firewall/', views.firewall, name='firewall'),
+
+]
